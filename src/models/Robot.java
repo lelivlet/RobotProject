6c921aa156c1c804aff3a6f89dfc366dc1ebba6a
Jorik <jorik71@gmail.com>
2018-06-05 16:08:41 +0200

    Auto stash before merge of "develop" and "origin/develop"

---
+++ src/models/Robot.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Robot {
+	Bewegingsapparaat bwApparaat = new Bewegingsapparaat();
+	ColorSensor CS = new ColorSensor();
+	
+	// Constructor
+	public Robot() {
+		super();
+	}
+	
+	
+
+}
