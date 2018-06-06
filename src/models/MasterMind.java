a3cbfa6e59e4500816f088e39e938e491a2e8437
Coco <huissoc@hva.nl>
2018-06-06 18:16:27 +0200

    Laatste mastermind update van mij vandaag

--- src/models/MasterMind.java
+++ src/models/MasterMind.java
@@ -16,28 +16,34 @@ public class MasterMind {
 		super();
 
 	}
-
-	public void speelMasterMind() {
-		System.out.println("Wil je mastermind spelen?");
-		waitForKey(Button.ENTER);
+	public int[] maakOplossing() {
 		int[] kleurenBrick = new int[4];
 		for (int i = 0; i < kleurenBrick.length; i++) {
 			kleurenBrick[i] = kleuren[(int) (Math.random() * 7)];
 		}
+	return kleurenBrick;
+		
+	}
+
+	public void speelMasterMind() {
+		System.out.println("Wil je mastermind spelen?");
+		waitForKey(Button.ENTER);
+		int [] oplossing=maakOplossing();
+		
 		while (goed[0] == false || goed[1] == false || goed[2] == false || goed[3] == false) {
-			for (int i = 0; i < kleurenBrick.length; i++) {
+			for (int i = 0; i < oplossing.length; i++) {
 				System.out.println("Aantal goed is " + aantalGoed);
-				System.out.println("Positie 1=" + goed[0] + "Positie 2=" + goed[1] + "Positie 3=" + goed[2]
-						+ "Positie 4=" + goed[3]);
+				System.out.println("Positie 1=" + goed[0] + "\nPositie 2=" + goed[1] + "\nPositie 3=" + goed[2]
+						+ "\nPositie 4=" + goed[3]);
 				System.out.println("Doe gok " + (i + 1) + " onder de sensor");
 				waitForKey(Button.ENTER);
 				gok[i] = CS.getColorID();
-				if (gok[i] == kleurenBrick[i]) {
+				if (gok[i] == oplossing[i]) {
 					System.out.println("Dat is goed");
 					goed[i] = true;
 					aantalGoed++;
-				} else if (gok[i] == kleurenBrick[0] || gok[i] == kleurenBrick[1] || gok[i] == kleurenBrick[2]
-						|| gok[i] == kleurenBrick[3]) {
+				} else if (gok[i] == oplossing[0] || gok[i] == oplossing[1] || gok[i] == oplossing[2]
+						|| gok[i] == oplossing[3]) {
 
 					System.out.println("Kleur is goed maar positie niet");
 
